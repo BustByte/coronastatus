ad40c4160ea1e32bfa1d4ef5f99e75201c085a1d
Eirik Fosse <eirik@bustbyte.no>
2020-03-15 23:57:07 +0100

    remove phone number from implementation

--- app/routes/report-routes.ts
+++ app/routes/report-routes.ts
@@ -1,24 +1,14 @@
 import express, { Request } from 'express';
 import { Symptom, CovidReport, Sex, TestResult } from '../domain/types';
 import { CovidReportRepository } from '../repository/CovidReportRepository';
-import { loginPinIssuer } from '../sms/loginPinIssuer';
-import { stripPhoneNumber, hashPhoneNumber } from '../sms/utils';
-import { SmsGatewayService } from '../sms/smsGatewayService';
-
-const {
-  SVEVE_USERNAME,
-  SVEVE_PASSWORD,
-  SHOULD_USE_SMS_LOGIN
-} = require('../../config.json');
 
 const router = express.Router();
 const reportRepo = new CovidReportRepository();
 
 router.get('/', (req, res) => {
-  const smsVerificationSuccess = req.query['success'] === 'true';
   res.locals.metaDescription =
     'Her kan du legge inn informasjon om din helsetilstand, slik at vi kan få en bedre oversikt over totalbildet i Norge.';
-  return res.render('pages/form', { smsVerificationSuccess });
+  return res.render('pages/report');
 });
 
 router.get('/numberOfReports', async (req, res) => {
@@ -43,13 +33,9 @@ const extractTestResult = (req: Request): TestResult | undefined => {
 router.post('/', async (req, res) => {
   const acceptPrivacyPolicy = req.body['accept-privacy-policy'] === 'on';
   if (!acceptPrivacyPolicy) {
-    return res.render('pages/form', { didNotAcceptPrivacyPolicy: true });
+    return res.render('pages/report', { didNotAcceptPrivacyPolicy: true });
   }
-  const phoneNumber = req.body['phone-number'];
-  const strippedPhoneNumber = stripPhoneNumber(phoneNumber);
-  const hashedPhoneNumber = hashPhoneNumber(strippedPhoneNumber);
   const covidReport: CovidReport = {
-    phoneNumber: hashedPhoneNumber,
     yearOfBirth: req.body['birth-year'],
     postalCode: req.body['postal-code'],
     hasBeenTested: req.body['been-tested'] === 'yes',
@@ -66,25 +52,12 @@ router.post('/', async (req, res) => {
       [Symptom.SORE_THROAT]: req.body['symptom-sore-throat'] === 'on'
     },
     hasBeenAbroadLastTwoWeeks: req.body['been-abroad'] === 'yes',
-    symptomStart: req.body['symptom-start']
+    symptomStart: req.body['symptom-start'],
+    submissionTimestamp: new Date().getTime()
   };
-  const pin = loginPinIssuer.issue(hashedPhoneNumber);
-
-  const gateway = new SmsGatewayService(
-    SVEVE_USERNAME,
-    SVEVE_PASSWORD,
-    SHOULD_USE_SMS_LOGIN
-  );
-  const success = await gateway.sendSmsWithPin(phoneNumber, pin);
-  if (success) {
-    reportRepo.addNewCovidReportForHashedPhoneNumber(
-      hashedPhoneNumber,
-      covidReport
-    );
-    return res.redirect(`/sms?nummer=${strippedPhoneNumber}`);
-  }
-  console.log('Sending SMS failed for ', covidReport);
-  return res.render('pages/form', { smsSendingFailed: true });
+  const passcode = 'ole-gunnar-solskjaer-123';
+  reportRepo.addNewCovidReport(passcode, covidReport);
+  return res.redirect(`/passcode?success=true`);
 });
 
 router.get('/elements', (req, res) => {
