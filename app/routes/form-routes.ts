9f42cacf72e3b7e83206bf13e4a4032760480b03
Eirik Fosse <eirik@bustbyte.no>
2020-03-15 12:38:47 +0100

    increase font size

--- app/routes/form-routes.ts
+++ app/routes/form-routes.ts
@@ -3,7 +3,16 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  res.locals.metaDescription =
+    'Her kan du legge inn informasjon om din helsetilstand, slik at vi kan få en bedre oversikt over totalbildet i Norge.';
   return res.render('pages/form');
+});
+
+router.post('/', (req, res) => {
+  const requestBody = {
+    fullName: req.body['full-name']
+  };
+  console.log(requestBody);
 });
 
 router.get('/elements', (req, res) => {
