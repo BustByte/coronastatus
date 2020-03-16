9058ab1db402dce212c44a9e487ff14b8bd1ad9e
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2020-03-16 15:43:08 +0100

    Only show latest reports for each passcode in map

--- app/routes/map-routes.ts
+++ app/routes/map-routes.ts
@@ -1,22 +1,23 @@
 import express from 'express';
 import { CovidReport, TestResult } from '../domain/types';
-import postalCodeCoordinates from '../domain/postalCodeCoordinates';
+import postalCodeCoordinates from '../util/postalCodeCoordinates';
 import { CovidReportRepository } from '../repository/CovidReportRepository';
+import { isShowingAtLeastOneSymptom } from '../util/report-aggregator';
 
 const router = express.Router();
 const reportRepo = new CovidReportRepository();
 
 router.get('/geojson', async (req, res) => {
-  const allReports = await reportRepo.getAllCovidReports();
+  const allReports = await reportRepo.getLatestCovidReports();
 
   const features: any[] =
     allReports.reduce((list: any[], report: CovidReport) => {
       const coordinates = postalCodeCoordinates(report.postalCode);
       if (coordinates.length !== 0) {
         let state = 'HEALTHY';
-        if (report.hasBeenTested && report.testResult === TestResult.POSITIVE) {
+        if (report.testResult === TestResult.POSITIVE) {
           state = 'POSITIVE';
-        } else if (Object.values(report.symptoms).includes(true)) {
+        } else if (isShowingAtLeastOneSymptom(report)) {
           state = 'SYMPTOMS';
         }
 
