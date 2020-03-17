b22f8c69eb57565f7d3539e7a18cd58e718cb2d0
Eirik Vikanes <eirik.vikanes@gmail.com>
2020-03-17 15:43:53 +0100

    Revert "Adds repository for municipalities"
    This reverts commit b12ae220ef1582e65a881a924f1bab82fe34f143.

--- app/routes/map-routes.ts
+++ app/routes/map-routes.ts
@@ -1,68 +1,43 @@
 import express from 'express';
-import { CovidReport, TestResult, Municipality } from '../domain/types';
+import { CovidReport, TestResult } from '../domain/types';
+import postalCodeCoordinates from '../util/postalCodeCoordinates';
 import { CovidReportRepository } from '../repository/CovidReportRepository';
-import { MunicipalityRepository } from '../repository/MunicipalityRepository';
 import { isShowingAtLeastOneSymptom } from '../util/report-aggregator';
 
 const router = express.Router();
 const reportRepo = new CovidReportRepository();
-const municipalityRepo = new MunicipalityRepository();
-
-interface MapFeature {
-  type: string;
-  geometry: {
-    type: string;
-    coordinates: number[];
-  };
-  properties: {
-    state: string;
-  };
-}
-
-const mapReportToFeatureState = (report: CovidReport): string => {
-  let state = 'HEALTHY';
-  if (report.hasBeenTested && report.testResult === TestResult.POSITIVE) {
-    state = 'POSITIVE';
-  } else if (isShowingAtLeastOneSymptom(report)) {
-    state = 'SYMPTOMS';
-  }
-  return state;
-};
-
-const mapFeatureFromMunicipalityAndReport = (
-  report: CovidReport,
-  municipality: Municipality
-): MapFeature => ({
-  type: 'Feature',
-  geometry: {
-    type: 'Point',
-    coordinates: [
-      Number(municipality.coordinates.lon),
-      Number(municipality.coordinates.lat)
-    ]
-  },
-  properties: {
-    state: mapReportToFeatureState(report)
-  }
-});
 
 router.get('/geojson', async (req, res) => {
   const allReports = await reportRepo.getLatestCovidReports();
-  const features = await Promise.all(
-    allReports.map(report =>
-      municipalityRepo
-        .getMunicipalityForPostalCode(report.postalCode)
-        .then(municipality =>
-          municipality
-            ? mapFeatureFromMunicipalityAndReport(report, municipality)
-            : undefined
-        )
-    )
-  );
+
+  const features: any[] =
+    allReports.reduce((list: any[], report: CovidReport) => {
+      const coordinates = postalCodeCoordinates(report.postalCode);
+      if (coordinates.length !== 0) {
+        let state = 'HEALTHY';
+        if (report.testResult === TestResult.POSITIVE) {
+          state = 'POSITIVE';
+        } else if (isShowingAtLeastOneSymptom(report)) {
+          state = 'SYMPTOMS';
+        }
+
+        list.push({
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates
+          },
+          properties: {
+            state
+          }
+        });
+      }
+      return list;
+    }, []) ?? [];
 
   res.send({
     type: 'FeatureCollection',
-    features: features.filter(feature => !!feature)
+    features
   });
 });
 
