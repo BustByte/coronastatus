d14600d1d4c7053a0aa671d7564af6990b4e5f69
Rina Andriana <rina@andriana.no>
2020-03-17 22:22:55 +0100

    Refactor height for hamburger menu

--- static/hamburger-menu.js
+++ static/hamburger-menu.js
@@ -4,14 +4,14 @@ hamburgerMenuButton.addEventListener('click', function() {
   var hamburgerMenu = document.getElementsByClassName('hamburger-menu')[0];
 
   if (hamburgerMenu.classList.contains('active')) {
-    hamburgerMenu.style.height = '';
+    hamburgerMenu.classList.remove('un-collapsed');
     setTimeout(function() {
       hamburgerMenu.classList.remove('active');
     }, 300);
   } else {
     hamburgerMenu.classList.add('active');
     setTimeout(function() {
-      hamburgerMenu.style.height = '140px';
+      hamburgerMenu.classList.add('un-collapsed');
     }, 1);
   }
 });
