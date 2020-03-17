ba62b1771d561ad3fd81f51c7058819db24074df
Sondre Hjetland <sondrehjetland@gmail.com>
2020-03-17 16:53:32 +0100

    Started data visualization

---
+++ app/routes/statistics-routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { CovidReportRepository } from '../repository/CovidReportRepository';
+
+const router = express.Router();
+const reportRepo = new CovidReportRepository();
+
+router.get('/data', async (req, res) => {
+  const allReports = await reportRepo.getLatestCovidReports();
+  res.send(allReports);
+});
+
+router.get('/', (req, res) => {
+  return res.render('pages/statistics');
+});
+
+export default router;
