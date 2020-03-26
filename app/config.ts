b6603576463366d5f6e2dba9bdea50e96f26ce4c
Francesco Zerbinati <zerbinati.francesco@gmail.com>
2020-03-26 13:49:39 +0100

    config with envs

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,9 @@
-import config from '../config.json';
+let config = {}
+try {  
+  config = require('../config.json');
+} catch (err) {
+  console.log('No config file found....fallback to env/defaults')
+}
 import { Config } from './domain/types';
 
 /**
@@ -8,17 +13,17 @@ import { Config } from './domain/types';
  * field is added
  * */
 const fallbackConfig: Config = {
-  BASE_URL: 'coronastatus.no',
-  LANGUAGE: 'no',
-  COUNTRY: 'Norway',
-  MAP_CENTER: '10.7522, 63.9139',
-  MAP_ZOOM: 4,
-  TWITTER: 'coronastatusNO',
-  ZIP_LENGTH: 4,
-  ZIP_PLACEHOLDER: '1234',
-  REDIRECT_TO_GOVERNMENT: false,
-  PASSCODE_LENGTH: 3,
-  DB_PATH: './covid_db'
+  BASE_URL: process.env.BASE_URL || 'coronastatus.no',
+  LANGUAGE: process.env.LANGUAGE || 'no',
+  COUNTRY: process.env.COUNTRY || 'Norway',
+  MAP_CENTER: process.env.BASE_URL || '10.7522, 63.9139',
+  MAP_ZOOM: parseInt(process.env.MAP_ZOOM || '4'),
+  TWITTER: process.env.TWITTER ||'coronastatusNO',
+  ZIP_LENGTH: parseInt(process.env.ZIP_LENGTH || '4'),
+  ZIP_PLACEHOLDER: process.env.ZIP_PLACEHOLDER || '1234',
+  REDIRECT_TO_GOVERNMENT: process.env.REDIRECT_TO_GOVERNMENT === 'true' ||  false,
+  PASSCODE_LENGTH: parseInt(process.env.PASSCODE_LENGTH || '3'),
+  DB_PATH: process.env.DB_PATH || './covid_db'
 };
 
 export default {
