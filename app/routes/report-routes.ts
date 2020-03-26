205c6b7f45552f5638ad6c2d2cdf532c0b3a3034
Francesco Zerbinati <zerbinati.francesco@gmail.com>
2020-03-26 17:27:09 +0100

    femove limit

--- app/routes/report-routes.ts
+++ app/routes/report-routes.ts
@@ -120,7 +120,7 @@ const toIsolationStatus = (inputValue: string): IsolationStatus | undefined => {
   return undefined;
 };
 
-router.post('/', createReportRateLimit, async (req, res) => {
+router.post('/', async (req, res) => {
   const acceptPrivacyPolicy = req.body['accept-privacy-policy'] === 'on';
   if (!acceptPrivacyPolicy) {
     const reports = await reportRepo.getLatestCovidReports();
