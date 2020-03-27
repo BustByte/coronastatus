9f0175c580d6675a18ea79243b381db69db04128
Kilian Valkhof <kilian@kilianvalkhof.com>
2020-03-27 09:39:58 +0100

    add puppeteer script to generate images

---
+++ scripts/generate-images.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const { LANGUAGE, LOCALE } = require('../config.json');
+
+const currentLanguage = LOCALE || LANGUAGE || 'en';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.setViewport({ width: 3000, height: 3000 });
+
+  await page.goto('http://localhost:7272/social-images').catch(e => {
+    console.log("ERROR: Run the server first, eg with `yarn dev` or 'npm run dev'.");
+    process.exit(1);
+  });
+
+  const ogImage = await page.$('.twitter-card');
+  await ogImage.screenshot({
+    path: `static/${currentLanguage}/social-media.png`
+  });
+
+  const twitterHeader = await page.$('.twitter-header');
+  await twitterHeader.screenshot({
+    path: `static/${currentLanguage}/twitter-header.png`
+  });
+
+  const banner = await page.$('.banner');
+  await banner.screenshot({ path: `static/${currentLanguage}/banner.png` });
+
+  await browser.close();
+})();
