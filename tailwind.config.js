7bba2ba19a60214ed8e07b744d1b2e812e5bcd66
Adriaan <adriaanvanrossum@users.noreply.github.com>
2020-03-29 17:30:12 +0200

    Add share options (#456)
    * Add share options
    
    * Add translations for every language in English
    
    * Add Danish
    
    * Add other languages via Google Translate
    
    * Add move lang
    
    * Delete commented code
    
    * Fix all English languages
    
    * Updated EN.json
    
    * Fix Canada
    
    * Update SK.json & fix whatsapp ref (#463)
    
    * Update SK.json
    
    * Fix whatsapp ref
    
    * Add spanish translations for share options (#460)
    
    * norwegian translations
    
    * english malaysian translations
    
    * Update spain line
    
    * Update tr.json
    
    * Add UA translation thanks to @tarasmatsyk
    
    * Missing Swedish translations (#466)
    
    * Missing Swedish translations
    
    * Removed duplicated, korona -> corona
    
    Co-authored-by: Pontus Freyhult <pontus_git@soua.net>
    
    Co-authored-by: Amrit Nagi <amrit@astrava.solutions>
    Co-authored-by: Stano Bo <sb@juniq.net>
    Co-authored-by: Julian Betancourt <julian@quiqup.com>
    Co-authored-by: Eirik Fosse <eirik@bustbyte.no>
    Co-authored-by: Berkay <btutal@users.noreply.github.com>
    Co-authored-by: Pontus Frehult <pontus.freyhult@nbis.se>
    Co-authored-by: Pontus Freyhult <pontus_git@soua.net>

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
         'cvs-green-light': '#e7f4f7',
         'cvs-green': '#cde2e2',
         'cvs-green-dark': '#2B7889',
-        'cvs-gold': '#F2CC20'
+        'cvs-gold': '#F2CC20',
+        'cvs-facebook': '#1877f2',
+        'cvs-facebook-dark': '#115cbd',
+        'cvs-twitter': '#1da1f2',
+        'cvs-twitter-dark': '#1171ac',
+        'cvs-whatsapp': '#1EBEA5',
+        'cvs-whatsapp-dark': '#169D88',
+        'cvs-linkedin': '#2867b2',
+        'cvs-linkedin-dark': '#1c4a82'
       }
     }
   },
@@ -16,4 +24,4 @@ module.exports = {
   plugins: [
     // Some useful comment
   ]
-}
+};
