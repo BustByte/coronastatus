0c19f3b88d13a797cd20724ffaa24b1a7413ed20
Adriaan <adriaanvanrossum@users.noreply.github.com>
2020-03-29 21:37:34 +0200

    Fix number formating and move function to server.ts (#479)
    * Fix number formating and move function to server.ts
    
    * Remove thousandSeparator

--- app/server.ts
+++ app/server.ts
@@ -74,7 +74,12 @@ app.use((req, res, next) => {
   res.locals.zipPattern = config.ZIP_PATTERN;
   res.locals.zipPlaceHolder = config.ZIP_PLACEHOLDER;
   res.locals.redirectToGovernment = config.REDIRECT_TO_GOVERNMENT;
-  res.locals.thousandSeparator = config.THOUSAND_SEPARATOR;
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  res.locals.formatNumber = x =>
+    x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, config.THOUSAND_SEPARATOR);
+
   next();
 });
 
