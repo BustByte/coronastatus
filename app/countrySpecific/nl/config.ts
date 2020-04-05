f3140f7b2de513e53faed40bc55353a1727fab13
Adriaan <adriaanvanrossum@users.noreply.github.com>
2020-04-05 20:53:42 +0200

    Update config.ts

--- app/countrySpecific/nl/config.ts
+++ app/countrySpecific/nl/config.ts
@@ -11,7 +11,7 @@ export const countrySpecificConfig: Config = {
   PASSCODE_LENGTH: 4,
   RATE_LIMIT_COUNT: 20, // Maximum number of submissions per IP
   RATE_LIMIT_WINDOW: 86400000, // Time window in milliseconds for rate limiting
-  REDIRECT_TO_GOVERNMENT: true,
+  REDIRECT_TO_GOVERNMENT: false,
   SUPPORTED_LOCALES: ['nl'],
   THOUSAND_SEPARATOR: '.',
   ZIP_GUIDE: false,
